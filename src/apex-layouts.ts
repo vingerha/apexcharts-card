2dd05b6c00af3aac0206009d843d30aa8f8ab374
Arjan <44190435+vingerha@users.noreply.github.com>
2025-07-21 16:49:15 +0200

    fix(l): more log

--- src/apex-layouts.ts
+++ src/apex-layouts.ts
@@ -443,6 +443,8 @@ function getLegendFormatter(config: ChartCardConfig, hass: HomeAssistant | undef
 			? opts.w.globals.series[opts.seriesIndex].slice(-1)[0]
 			: opts.w.globals.series[opts.seriesIndex];
 		if (offSet && (inLegend === 'after_now' || inLegend === 'before_now')) {
+			console.warn('getLegendFormatter opts1: ', opts.w.globals.series[opts.seriesIndex]);
+			console.warn('getLegendFormatter opts2: ', opts.w.globals.series[opts.seriesIndex][dataPointIndex]);
 			value = getLastValueBeforeNowWithOffset(opts.w.globals.series[opts.seriesIndex], offSet)
 		}
 		if (conf.series_in_graph[opts.seriesIndex]?.invert && value) {
